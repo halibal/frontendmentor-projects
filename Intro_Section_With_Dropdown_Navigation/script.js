05a759342643689e113f0cbb6bf724e627d92e55
Halibal <halibal95@gmail.com>
2022-05-16 16:51:04 +0300

    project is about to be completed

--- Intro_Section_With_Dropdown_Navigation/script.js
+++ Intro_Section_With_Dropdown_Navigation/script.js
@@ -2,13 +2,22 @@
 const menuIcon = document.getElementById('menuIcon');
 const sidebar = document.querySelector('.sidebar');
 const sidebarClose = document.getElementById('sidebarClose');
-const features = document.getElementById('features');
-const dropdownFeatures = document.querySelector('.dropdownFeatures');
+
+// desktop
 const company = document.getElementById('company');
+const features = document.getElementById('features');
 const dropdownCompany = document.querySelector('.dropdownCompany');
+const dropdownFeatures = document.querySelector('.dropdownFeatures');
 const companyArrow = document.getElementById('companyArrow');
 const featuresArrow = document.getElementById('featuresArrow');
 
+// mobile
+const featuresMobile = document.getElementById('featuresMobile');
+const companyMobile = document.getElementById('companyMobile');
+const dropdownCompanyMobile = document.querySelector('.dropdownCompanyMobile');
+const dropdownFeaturesMobile = document.querySelector('.dropdownFeaturesMobile');
+const companyArrowMobile = document.getElementById('companyArrowMobile');
+const featuresArrowMobile = document.getElementById('featuresArrowMobile');
 // DOM ends
 
 
@@ -33,7 +42,7 @@ function openSideBar() {
 
 
 
-// 
+// desktop
 
 features.addEventListener('click', openFeaturesDropDown);
 company.addEventListener('click', openCompanyDropDown);
@@ -43,9 +52,13 @@ function openFeaturesDropDown() {
     if (dropdownFeatures.classList.contains('hide')) {
         dropdownFeatures.classList.remove('hide');
         featuresArrow.classList.add('upsideDown');
+        dropdownCompany.classList.add('hide');
+        companyArrow.classList.remove('upsideDown');
     } else {
         dropdownFeatures.classList.add('hide');
         featuresArrow.classList.remove('upsideDown');
+        dropdownCompany.classList.add('hide');
+        companyArrow.classList.remove('upsideDown');
     }
 }
 
@@ -53,8 +66,45 @@ function openCompanyDropDown() {
     if (dropdownCompany.classList.contains('hide')) {
         dropdownCompany.classList.remove('hide');
         companyArrow.classList.add('upsideDown');
+        dropdownFeatures.classList.add('hide');
+        featuresArrow.classList.remove('upsideDown');
     } else {
         dropdownCompany.classList.add('hide');
         companyArrow.classList.remove('upsideDown');
+        dropdownFeatures.classList.add('hide');
+        featuresArrow.classList.remove('upsideDown');
+    }
+}
+
+// mobile
+
+featuresMobile.addEventListener('click', openFeaturesDropDownMobile);
+companyMobile.addEventListener('click', openCompanyDropDownMobile);
+
+function openFeaturesDropDownMobile() {
+    if (dropdownFeaturesMobile.classList.contains('hide')) {
+        dropdownFeaturesMobile.classList.remove('hide');
+        featuresArrowMobile.classList.add('upsideDown');
+        dropdownCompanyMobile.classList.add('hide');
+        companyArrowMobile.classList.remove('upsideDown');
+    } else {
+        dropdownFeaturesMobile.classList.add('hide');
+        featuresArrowMobile.classList.remove('upsideDown');
+        dropdownCompanyMobile.classList.add('hide');
+        companyArrowMobile.classList.remove('upsideDown');
+    }
+}
+
+function openCompanyDropDownMobile() {
+    if (dropdownCompanyMobile.classList.contains('hide')) {
+        dropdownCompanyMobile.classList.remove('hide');
+        companyArrowMobile.classList.add('upsideDown');
+        dropdownFeaturesMobile.classList.add('hide');
+        featuresArrowMobile.classList.remove('upsideDown');
+    } else {
+        dropdownCompanyMobile.classList.add('hide');
+        companyArrowMobile.classList.remove('upsideDown');
+        dropdownFeaturesMobile.classList.add('hide');
+        featuresArrowMobile.classList.remove('upsideDown');
     }
 }
